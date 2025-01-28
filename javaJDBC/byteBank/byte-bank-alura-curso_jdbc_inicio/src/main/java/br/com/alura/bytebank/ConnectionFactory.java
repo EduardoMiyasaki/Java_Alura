9673cee31ffd1eff0de6449ac5e4f3a9f172bea2
EduardoMiyasaki <eduardomiyasaki22@gmail.com>
2025-01-28 19:51:54 -0300

    Subindo arquivo no gitIgnore

--- javaJDBC/byteBank/byte-bank-alura-curso_jdbc_inicio/src/main/java/br/com/alura/bytebank/ConnectionFactory.java
+++ javaJDBC/byteBank/byte-bank-alura-curso_jdbc_inicio/src/main/java/br/com/alura/bytebank/ConnectionFactory.java
@@ -9,7 +9,7 @@ public class ConnectionFactory {
     public Connection openConnection() {
         try {
             return DriverManager
-                    .getConnection("jdbc:mysql://localhost:3306/byte_bank?user=root&password=");
+                    .getConnection("jdbc:mysql://localhost:3306/byte_bank?user=root&password=10062006Dudu");
         } catch (SQLException e) {
             throw new RuntimeException(e.getMessage());
         }
@@ -21,7 +21,7 @@ public class ConnectionFactory {
         // fazendo a conexão com o banco
         try {
             // Connection serve para guardar a variavel de conexao do conexão MySQL
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/byte_bank?user=root&password=");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/byte_bank?user=root&password=10062006Dudu");
             System.out.println("Conexão bem sucedida");
             connection.close();
         } catch (SQLException e) {
