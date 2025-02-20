1a7bf930d5df5486a11d4aa9a438aacb0d6627b6
EduardoMiyasaki <eduardomiyasaki22@gmail.com>
2025-02-20 12:44:42 -0300

    ;

---
+++ SpringBoot/Exercicios1/demo/src/main/java/br/com/alura/exercicio1/demo/exercicio2/MultiplicadoPorTres.java
@@ -0,0 +1,8 @@
+package br.com.alura.exercicio1.demo.exercicio2;
+
+import java.util.List;
+
+public interface MultiplicadoPorTres {
+
+    List<Integer> multiplicarPor3(List<Integer> a);
+}
---
+++ SpringBoot/Exercicios1/demo/src/main/java/br/com/alura/exercicio1/demo/exercicio2/Multiplicao.java
@@ -0,0 +1,6 @@
+package br.com.alura.exercicio1.demo.exercicio2;
+
+public class Multiplicao implements Multiplicacao{
+
+  soma 
+}
